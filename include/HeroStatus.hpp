ae62db9d234ab336a4a5e83f50fef7ada057dff9
Kilian Brendel <kilian.brendel@posteo.de>
2020-08-29 19:53:26 +0200

    Added HeroStatus toString helper

--- include/HeroStatus.hpp
+++ include/HeroStatus.hpp
@@ -17,13 +17,46 @@ enum class HeroStatus
   Weakened
 };
 
-inline bool canHaveMultiple(HeroStatus status)
+constexpr const char* toString(HeroStatus status)
+{
+  switch (status)
+  {
+  case HeroStatus::BloodCurse:
+    return "Blood Curse";
+  case HeroStatus::Corrosion:
+    return "Corrosion";
+  case HeroStatus::Cursed:
+    return "Cursed";
+  case HeroStatus::ExperienceBoost:
+    return "Experience Boost";
+  case HeroStatus::FirstStrike:
+    return "First Strike";
+  case HeroStatus::Humility:
+    return "Humility";
+  case HeroStatus::Learning:
+    return "Learning";
+  case HeroStatus::MagicalAttack:
+    return "Magical Attack";
+  case HeroStatus::ManaBurn:
+    return "Mana Burn";
+  case HeroStatus::Poisoned:
+    return "Poisoned";
+  case HeroStatus::Reflexes:
+    return "Reflexes";
+  case HeroStatus::SlowStrike:
+    return "Slow Strike";
+  case HeroStatus::Weakened:
+    return "Weakened";
+  }
+}
+
+constexpr bool canHaveMultiple(HeroStatus status)
 {
   return status == HeroStatus::Corrosion || status == HeroStatus::Cursed || status == HeroStatus::Learning ||
          status == HeroStatus::Weakened;
 }
 
-inline bool isRemovable(HeroStatus status)
+constexpr bool isRemovable(HeroStatus status)
 {
   return status != HeroStatus::BloodCurse && status != HeroStatus::Humility;
 }
