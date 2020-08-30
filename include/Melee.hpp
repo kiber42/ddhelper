51149ea1f92a2c1e24281eb2513d05fd380707e7
Kilian Brendel <kilian.brendel@posteo.de>
2020-08-30 07:57:12 +0200

    Pretty printing for melee outcome summary

--- include/Melee.hpp
+++ include/Melee.hpp
@@ -3,6 +3,8 @@
 #include "Hero.hpp"
 #include "Monster.hpp"
 
+#include <ostream>
+
 struct Outcome
 {
   enum class Summary
@@ -17,6 +19,25 @@ struct Outcome
   Hero hero;
   Monster monster;
 };
+
+constexpr const char* toString(Outcome::Summary status)
+{
+  using Summary = Outcome::Summary;
+  switch (status)
+  {
+    case Summary::Safe: return "Safe";
+    case Summary::HeroWins: return "Win";
+    case Summary::HeroDefeated: return "Death";
+    case Summary::HeroDebuffed: return "Debuffed";
+    default:
+    case Summary::Error: return "ERROR";
+  }
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Outcome::Summary& summary)
+{
+  return os << toString(summary);
+}
 
 namespace Melee
 {
