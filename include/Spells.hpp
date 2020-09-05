8d47894bc8eb087c3872f2a2f398bcae8da75bbf
Kilian Brendel <kilian.brendel@posteo.de>
2020-09-05 15:54:09 +0200

    Pretty printing for spells

--- include/Spells.hpp
+++ include/Spells.hpp
@@ -21,6 +21,39 @@ enum class Spell
   Wonafyt
 };
 
+constexpr const char* toString(Spell spell)
+{
+  switch (spell)
+  {
+  case Spell::Apheelsik:
+    return "Apheelsik";
+  case Spell::Bludtupowa:
+    return "Bludtupowa";
+  case Spell::Burndayraz:
+    return "Burndayraz";
+  case Spell::Bysseps:
+    return "Bysseps";
+  case Spell::Cydstepp:
+    return "Cydstepp";
+  case Spell::Endiswal:
+    return "Endiswal";
+  case Spell::Getindare:
+    return "Getindare";
+  case Spell::Halpmeh:
+    return "Halpmeh";
+  case Spell::Imawal:
+    return "Imawal";
+  case Spell::Lemmisi:
+    return "Lemmisi";
+  case Spell::Pisorf:
+    return "Pisorf";
+  case Spell::Weytwut:
+    return "Weytwut";
+  case Spell::Wonafyt:
+    return "Wonafyt";
+  }
+}
+
 namespace Cast
 {
   // Determine whether spell can currently be cast
