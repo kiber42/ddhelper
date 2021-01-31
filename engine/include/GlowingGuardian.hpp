8956094c70936f108ddc84e55ad7b425827a04dd
Kilian Brendel <kilian.brendel@posteo.de>
2021-01-31 20:44:25 +0100

    Add glowing guardian absolution boon logic

---
+++ engine/include/GlowingGuardian.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Monster.hpp"
+
+#include <random>
+
+class GlowingGuardian
+{
+  public:
+    GlowingGuardian();
+    bool canUseAbsolution(int heroLevel, const Monsters& monsters) const;
+    Monsters::iterator pickMonsterForAbsolution(int heroLevel, Monsters& monsters);
+  private:
+    std::mt19937 generator;
+};
---
+++ engine/src/GlowingGuardian.cpp
@@ -0,0 +1,29 @@
+#include "GlowingGuardian.hpp"
+
+#include <algorithm>
+
+GlowingGuardian::GlowingGuardian()
+  : generator(std::random_device{}())
+{
+}
+
+bool GlowingGuardian::canUseAbsolution(int heroLevel, const Monsters& monsters) const
+{
+  return std::find_if(begin(monsters), end(monsters), [maxLevel = std::min(heroLevel, 9)](auto& monster) {
+           return monster.getLevel() <= maxLevel && !monster.isUndead();
+         }) != end(monsters);
+}
+
+Monsters::iterator GlowingGuardian::pickMonsterForAbsolution(int heroLevel, Monsters& monsters)
+{
+  std::vector<Monsters::iterator> lowerLevelMonsters;
+  const int maxLevel = std::min(heroLevel, 9);
+  for (auto monsterIt = begin(monsters); monsterIt != end(monsters); ++monsterIt)
+  {
+    if (monsterIt->getLevel() <= maxLevel && !monsterIt->isUndead())
+      lowerLevelMonsters.emplace_back(monsterIt);
+  }
+  if (lowerLevelMonsters.empty())
+    return end(monsters);
+  return lowerLevelMonsters[std::uniform_int_distribution<>(0, lowerLevelMonsters.size() - 1)(generator)];
+}
